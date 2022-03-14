9c43d49d8ddbcd11a500f95e5314d2b8012c71f5
carloscoelhogti <77030632+carloscoelhogti@users.noreply.github.com>
2022-03-14 13:24:07 -0400

    Update ajax.js

--- AUTOMACAO/scripts/ajax.js
+++ AUTOMACAO/scripts/ajax.js
@@ -3,6 +3,6 @@ function loadDoc() {
     xhttp.onload = function() {
       document.getElementById("demo").innerHTML = this.responseText;
       }
-    xhttp.open("GET", "MCABalEntrada.txt", true);
+    xhttp.open("GET", "https://github.com/carloscoelhogti/in/blob/main/AUTOMACAO/MCABalEntrada.txt", true);
     xhttp.send();
   }
