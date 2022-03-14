018c72e1fdbea57ba7e82a9219b4448ff7fb52cd
carloscoelhogti <77030632+carloscoelhogti@users.noreply.github.com>
2022-03-14 13:48:46 -0400

    Update files.js

--- AUTOMACAO/scripts/files.js
+++ AUTOMACAO/scripts/files.js
@@ -1,7 +1,7 @@
 //Efetuando a leitura do arquivo
 {
     let fs = require('fs'); //variavel deve ser criada para que possamos ler o arquivo
-    fs.readFile("MCABalEntrada.txt",'utf8', function(err,data){
+    fs.readFile("AUTOMACAO/Report/MCABalEntrada.txt",'utf8', function(err,data){
         //Enviando para o console o resultado da leitura
         console.log(data);
     });
@@ -9,7 +9,7 @@
     
     {
     let fs = require('fs'); //variavel deve ser criada para que possamos ler o arquivo
-    fs.readFile("c:/ANTENABALENTLADOA.txt",'utf8', function(err,data){
+    fs.readFile("AUTOMACAO/Report/ANTENABALENTLADOA.txt",'utf8', function(err,data){
         //Enviando para o console o resultado da leitura
         console.log(data);
     });
@@ -17,7 +17,7 @@
     
     {
     let fs = require('fs'); //variavel deve ser criada para que possamos ler o arquivo
-    fs.readFile("c:/ANTENABALENTLADOB.txt",'utf8', function(err,data){
+    fs.readFile("AUTOMACAO/Report/ANTENABALENTLADOB.txt",'utf8', function(err,data){
         //Enviando para o console o resultado da leitura
         console.log(data);
     });
