0c72fbc5c632f770c562ab1e4f96acc0059f73f1
carloscoelhogti <77030632+carloscoelhogti@users.noreply.github.com>
2022-05-24 13:06:37 -0400

    Add files via upload

---
+++ Scripts/index.js
@@ -0,0 +1,17 @@
+console.log("Testando script externo");
+
+function clickBotao(){
+    alert("Hello Dev!")
+    document.getElementById("ParagrafoTeste").innerHTML="Texto Alterado via Botão";//altera texto no elemento
+}
+
+function DesligarLampada(){
+    document.getElementById("lampada").src ="./Images/lampadaapagada.gif";//altera imagem do img
+    //alert("Lampada Desligada");//exibe alerta na pagina
+}
+
+function LigarLampada(){
+    document.getElementById("lampada").src = "./Images/lampadaviva.gif"
+    //alert("Lampada Ligada")
+}
+
