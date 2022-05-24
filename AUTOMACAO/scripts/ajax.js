a69d6b6c0e092b956b27d3264aa1e31c38565d4d
carloscoelhogti <77030632+carloscoelhogti@users.noreply.github.com>
2022-05-24 09:20:31 -0400

    Delete AUTOMACAO directory

--- AUTOMACAO/scripts/ajax.js
+++
@@ -1,8 +0,0 @@
-function loadDoc() {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onload = function() {
-      document.getElementById("demo").innerHTML = this.responseText;
-      }
-    xhttp.open("GET", "https://github.com/carloscoelhogti/in/blob/main/AUTOMACAO/MCABalEntrada.txt", true);
-    xhttp.send();
-  }
